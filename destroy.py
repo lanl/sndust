06a4adb3f1e1a51813380bb996872539388ead9d
Sarah Stangl <stangl@django.nhn.ou.edu>
2021-06-04 10:56:51 -0500

    tried to add gas update --- need to add somthing for components --- kinda works

--- destroy.py
+++ destroy.py
@@ -10,6 +10,10 @@ from atomic_mass import *
 import numba
 from numba import jit
 import time
+from erosionDict import *
+from particle import *
+from network import *
+from gas import *
 
 onethird = 1./3.
 twothird = 2. * onethird
@@ -24,31 +28,33 @@ g2amu = 6.022e+23
 amu2g = 1. / g2amu
 JtoEV = 6.242e+18
 
-def destroy(p, net):
+def destroy(g: SNGas, p: Particle, net: Network):
+    volume = p.volume
     T = p.temperatures
     vc = p.velocity
-    species = list(net._species)[:3]
-    abun_list = np.asarray([p.composition[s]  for s in species])
+    species = list(p.composition.keys())
+    abun_list = list(p.composition.values())
     n_tot = sum([abun_list[Sidx] * AMU[s.strip()] for Sidx,s in enumerate(species)])
     grain_names = net._species_dust
     dest = np.zeros((len(T),16))
+    print('one call')
     for i in list(range(len(T))):
-        dec = calc_TOTAL_dadt(grain_names,T[i],n_tot,abun_list,species,vc[i]) / 1E4
+        dec = calc_TOTAL_dadt(grain_names,T[i],n_tot,abun_list,species,vc[i],p,g,net,volume[i]) / 1E4
         dest[i] = dec
     return dest
 
 #will need to pass in an array or dictionary or all the abundances
-def calc_TOTAL_dadt(grain_list,T,n,abun,abun_name,vc):
+def calc_TOTAL_dadt(grain_list,T,n,abun,abun_name,vc,p: Particle,g: SNGas,net: Network,volume):
     destruct_list = np.zeros(len(grain_list))
     vd = vc / 100000
     si = np.sqrt( (vd ** 2) / (2 * kB_erg * T))
     if si > 10:
-        return non_THERMAL_dadt(grain_list,T,n,abun,abun_name,vd)
+        return non_THERMAL_dadt(grain_list,T,n,abun,abun_name,vd,p,g,net,volume)
     else:
-        return THERMAL_dadt(grain_list,T,n,abun,abun_name)
+        return THERMAL_dadt(grain_list,T,n,abun,abun_name,p,g,net,volume)
 
 #will need to pass in an array or dictionary or all the abundances
-def THERMAL_dadt(grain_list,T,n,abun,abun_name):
+def THERMAL_dadt(grain_list,T,n,abun,abun_name,p: Particle,g: SNGas,net: Network,volume):
     destruct_list = np.zeros(len(grain_list))
     for GRidx,grain in enumerate(grain_list):
         grain = str(grain.replace('(s)',''))
@@ -63,15 +69,15 @@ def THERMAL_dadt(grain_list,T,n,abun,abun_name):
             ## these two lines take forever
             start = time.time()
             yp = Yield(u0 = v["u0"],md = v["md"],mi = ions[i_abun_name]["mi"],zd = v["zd"],zi = ions[i_abun_name]["zi"],K = v["K"])
-
+            sidx = net.sidx(grain)
+            g._c0[sidx] = g._c0[sidx] + yp.Y(x * kB_eV * T)/volume
             dadt += pref * quad(lambda x: x * np.exp(-x) * yp.Y(x * kB_eV * T), a=yp.eth/(kB_eV * T) , b=np.infty)[0]
-            print(time.time()-start)
         dadt *= (v["md"] * amu2g) / (2. * v["rhod"]) * n
         destruct_list[GRidx] = dadt
     return destruct_list
 
 #will need to pass in an array or dictionary or all the abundances
-def non_THERMAL_dadt(grain_list,T,n,abun,abun_name,vd):
+def non_THERMAL_dadt(grain_list,T,n,abun,abun_name,vd,p: Particle,g: SNGas,net: Network,volume):
     destruct_list = np.zeros(len(grain_list))
     for GRidx,grain in enumerate(grain_list):
         grain = str(grain.replace('(s)',''))
@@ -85,7 +91,8 @@ def non_THERMAL_dadt(grain_list,T,n,abun,abun_name,vd):
             pref = val
             x = 1./2. * ions[i_abun_name]["mi"] * amu2g / 1000 * np.power(vd / 1000,2) * JtoEV
             yp = Yield(u0 = v["u0"],md = v["md"],mi = ions[i_abun_name]["mi"],zd = v["zd"],zi = ions[i_abun_name]["zi"],K = v["K"])
-
+            sidx = net.sidx(grain)
+            g._c0[sidx] = g._c0[sidx] + yp.Y(x)/volume
             dadt += pref * yp.Y(x)
         dadt *= (v["md"] * amu2g * vd) / (2. * v["rhod"]) * n
         destruct_list[int(GRidx)] = dadt
