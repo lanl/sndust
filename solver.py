c97dd8f2625a36a575a97c187c78bdf468f4d6b1
Sarah Stangl <stangl@django.nhn.ou.edu>
2021-07-23 13:05:31 -0500

    removed shock check in solver - wasn't updating y, idk why, but it works now

--- solver.py
+++ solver.py
@@ -9,6 +9,7 @@ import simulation_constants as sim_const
 from simulation_constants import *
 from stepper import Stepper
 from observer import Observer
+from printer import *
 
 kB_erg = 1.380649E-16
 
@@ -44,21 +45,6 @@ class Solver(object):
             # reset timer and take timestep
             _xtime0 = default_timer()
             msg = self._ode.step()
-
-            # check for shock
-            self.shock = self._stepper._gas._fS(self._ode.t)
-
-            if self.shock >= 0.5:
-                t = self._ode.t
-                rho = self._stepper._gas.Density(t)
-                T = self._stepper._gas.Temperature(t)
-                gas_name = list(self._stepper._net._species_gas)
-                gas_conc = self._stepper._gas._c0[:self._stepper._net.NG]
-                n_tot = sum([gas_conc[Sidx] * AMU[s.strip()] for Sidx,s in enumerate(gas_name)])
-                press = n_tot * kB_erg * T
-                # assume diatomic molecules for gas gamma = 7/5
-                v_shock = np.sqrt(7.0/5.0 *press/rho)
-                self._ode.y[self._stepper._net._NG + self._stepper._net._ND * N_MOMENTS + self._stepper._net._ND * numBins: -1] = 3.0/4.0 * v_shock
 
             self._tot_steptime += (default_timer() - _xtime0)
             self._avg_steptime = (self._tot_steptime / float(self._steps+1))
